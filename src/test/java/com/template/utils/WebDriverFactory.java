4e817d48d422dd7e90cb2b8f56831ddce0b446be
Cosme Assis <cosme.assis.base2@araujo.com.br>
2025-03-17 14:32:43 -0300

    retirada do docker

--- src/test/java/com/template/utils/WebDriverFactory.java
+++ src/test/java/com/template/utils/WebDriverFactory.java
@@ -1,12 +1,13 @@
 package com.template.utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.time.Duration;
 
 public class WebDriverFactory {
     private static WebDriver driver;
@@ -14,31 +15,31 @@ public class WebDriverFactory {
     public static WebDriver getDriver(String browser) {
         if (driver == null) {
             boolean isHeadless = Boolean.parseBoolean(System.getProperty("headless", "true"));
-            String seleniumServerUrl = System.getenv("SELENIUM_REMOTE_URL");
 
-            try {
-                switch (browser.toLowerCase()) {
-                    case "firefox":
-                        FirefoxOptions firefoxOptions = new FirefoxOptions();
-                        if (isHeadless) {
-                            firefoxOptions.addArguments("--headless");
-                        }
-                        driver = new RemoteWebDriver(new URL(seleniumServerUrl), firefoxOptions);
-                        break;
+            switch (browser.toLowerCase()) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    if (isHeadless) {
+                        firefoxOptions.addArguments("--headless");
+                    }
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
 
-                    default:
-                        ChromeOptions chromeOptions = new ChromeOptions();
-                        if (isHeadless) {
-                            chromeOptions.addArguments("--headless");
-                            chromeOptions.addArguments("--disable-gpu");
-                            chromeOptions.addArguments("--window-size=1920,1080");
-                        }
-                        driver = new RemoteWebDriver(new URL(seleniumServerUrl), chromeOptions);
-                        break;
-                }
-            } catch (MalformedURLException e) {
-                throw new RuntimeException("URL do Selenium Server está incorreta: " + seleniumServerUrl, e);
+                default: // Chrome (padrão)
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (isHeadless) {
+                        chromeOptions.addArguments("--headless");
+                        chromeOptions.addArguments("--disable-gpu");
+                        chromeOptions.addArguments("--window-size=1920,1080");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
+                    break;
             }
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
         return driver;
     }
